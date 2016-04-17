0f18e991cf94b6d987447ba91acd28e66aa4f599
Lancerpilgrim <lancerazure@gmail.com>
2016-04-17 17:36:12 +0800

    Update factory_method.py
    the old one is too simple.

--- factory_method.py
+++ factory_method.py
@@ -3,43 +3,115 @@
 
 """http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/"""
 
-
-class GreekGetter:
-
-    """A simple localizer a la gettext"""
-
-    def __init__(self):
-        self.trans = dict(dog="σκύλος", cat="γάτα")
-
-    def get(self, msgid):
-        """We'll punt if we don't have a translation"""
-        try:
-            return self.trans[msgid]
-        except KeyError:
-            return str(msgid)
-
-
-class EnglishGetter:
-
-    """Simply echoes the msg ids"""
-
-    def get(self, msgid):
-        return str(msgid)
-
-
-def get_localizer(language="English"):
-    """The factory method"""
-    languages = dict(English=EnglishGetter, Greek=GreekGetter)
-    return languages[language]()
-
-# Create our localizers
-e, g = get_localizer(language="English"), get_localizer(language="Greek")
-# Localize some text
-for msgid in "dog parrot cat bear".split():
-    print(e.get(msgid), g.get(msgid))
-
-### OUTPUT ###
-# dog σκύλος
-# parrot parrot
-# cat γάτα
-# bear bear
+#!/usr/bin/python  
+  
+  
+class Pizza:  
+    name = ""  
+    dough = ""  
+    sauce = ""  
+    toppings = []  
+  
+    def prepare(self):  
+        print "Preparing %s" % self.name  
+        print "    dough: %s" % self.dough  
+        print "    sauce: %s" % self.sauce  
+        print "    add toppings:"  
+        for n in self.toppings:  
+            print "        %s" % n  
+  
+    def bake(self):  
+        print "Bake for 25 minutes at 350."  
+  
+    def cut(self):  
+        print "Cutting into diagonal slices."  
+  
+    def box(self):  
+        print "Put into official box."  
+  
+    def get_name(self):  
+        return self.name  
+  
+  
+class PizzaStore:  
+    def order_pizza(self, pizza_type):  
+        self.pizza = self.create_pizza(pizza_type)  
+        self.pizza.prepare()  
+        self.pizza.bake()  
+        self.pizza.cut()  
+        self.pizza.box()  
+        return self.pizza  
+  
+    def create_pizza(self, pizza_type):  
+        pass  
+  
+  
+class NYStyleCheesePizza(Pizza):  
+    def __init__(self):  
+        self.name = "NY Style Cheese Pizza"  
+        self.dough = "NY Dough"  
+        self.sauce = "NY Sauce"  
+        self.toppings.append("NY toopping A")  
+        self.toppings.append("NY toopping B")  
+  
+  
+class ChicagoStyleCheesePizza(Pizza):  
+    def __init__(self):  
+        self.name = "Chicago Style Cheese Pizza"  
+        self.dough = "Chicago Dough"  
+        self.sauce = "Chicago Sauce"  
+        sefl.toppings.append("Chicago toopping A")  
+  
+    def cut(self):  
+        print "Cutting into square slices."  
+  
+  
+class NYStyleClamPizza(Pizza):  
+    def __init__(self):  
+        self.name = "NY Style Clam Pizza"  
+        self.dough = "NY Dough"  
+        self.sauce = "NY Sauce"  
+        self.toppings.append("NY toopping A")  
+        self.toppings.append("NY toopping B")  
+  
+  
+class ChicagoStyleClamPizza(Pizza):  
+    def __init__(self):  
+        self.name = "Chicago Style Clam Pizza"  
+        self.dough = "Chicago Dough"  
+        self.sauce = "Chicago Sauce"  
+        self.toppings.append("Chicago toopping A")  
+  
+    def cut(self):  
+        print "Cutting into square slices."  
+  
+  
+class NYPizzaStore(PizzaStore):  
+    def create_pizza(self, pizza_type):  
+        if pizza_type == "cheese":  
+            return NYStyleCheesePizza()  
+        elif pizza_type == "clam":  
+            return NYStyleClamPizza()  
+        else:  
+            return None  
+  
+  
+class ChicagoPizzaStore(PizzaStore):  
+    def create_pizza(self, pizza_type):  
+        if pizza_type == "cheese":  
+            return ChicagoStyleCheesePizza()  
+        elif pizza_type == "clam":  
+            return ChicagoStyleClamPizza()  
+        else:  
+            return None  
+  
+if __name__ == "__main__":  
+    ny_store = NYPizzaStore()  
+    chicago_store = ChicagoPizzaStore()  
+  
+    pizza = ny_store.order_pizza("cheese")  
+    print "Mike ordered a %s." % pizza.get_name()  
+    print   
+  
+    pizza = chicago_store.order_pizza("clam")  
+    print "John ordered a %s." % pizza.get_name()  
